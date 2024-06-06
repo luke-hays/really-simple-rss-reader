84f3e6b07052627f2c5ebae95d9e884f2178224b
Midival <lukehays32@gmail.com>
2024-06-06 17:06:31 -0400

    refactor accordion creation logic into class

---
+++ src/accordion.ts
@@ -0,0 +1,95 @@
+// Spec according to W3 for practice
+// This may be transformed to a web component - for right now just rely on styling outside
+export default class CustomAccordion {
+  triggerContent : string | Node
+  panelContent : string | Node
+  hidden? : boolean = true // Collapsed would be a more a more appropriate term
+  accordion : HTMLDivElement = document.createElement('div')
+
+  #id : string
+  #header : HTMLHeadingElement = document.createElement('h4')
+  #trigger : HTMLButtonElement = document.createElement('button')
+  #panel : HTMLDivElement = document.createElement('div')
+
+  constructor(
+    triggerContent : string | Node, 
+    panelContent: string | Node, 
+    hidden? : boolean
+  ) {
+    this.#id = crypto.randomUUID()
+    this.triggerContent = triggerContent
+    this.panelContent = panelContent
+    this.hidden = hidden
+
+    this
+      .#createHeader()
+      .#createTrigger()
+      .#createPanel()
+
+    return this
+  }
+
+  #createHeader() {
+    this.#header.classList.add('accordion-header')
+    this.accordion.appendChild(this.#header)
+
+    return this
+  }
+
+  #createTrigger() {
+    this.#trigger.classList.add('accordion-trigger')
+    this.#trigger.id = `${this.#id}-accordion`
+    this.#trigger.setAttribute('aria-controls', this.#id)
+    this.#trigger.setAttribute('aria-expanded', "true")
+
+    // Uncaught TypeError TypeError: Cannot read private member #panel from an object whose class did not declare it
+    // Probably due to passing it in as an event handler, bind this to properly call
+    this.#trigger.addEventListener('click', this.#toggleExpanded.bind(this))
+
+    if (typeof this.triggerContent === 'string') {
+      this.#trigger.textContent = this.triggerContent
+    } else {
+      this.#trigger.appendChild(this.triggerContent)
+    }
+
+    if (this.hidden) {
+      this.#trigger.setAttribute('aria-expanded', "false")
+    }
+
+    this.#header.appendChild(this.#trigger)
+
+    return this
+  }
+
+  #createPanel() {
+    this.#panel.classList.add('accordion-panel')
+    this.#panel.id = this.#id
+    this.#panel.setAttribute('aria-expanded', "true")
+    this.#panel.setAttribute('aria-labelledby', `${this.#id}-accordion`)
+
+    if (typeof this.panelContent === 'string') {
+      this.#panel.textContent = this.panelContent
+    } else {
+      this.#panel.appendChild(this.panelContent)
+    }
+
+    if (this.hidden) {
+      this.#panel.setAttribute('aria-expanded', "false")
+      this.#panel.toggleAttribute('hidden')
+    }
+
+    this.accordion.appendChild(this.#panel)
+  
+    return this
+  }
+
+  #toggleExpanded() {
+    this.hidden = !this.hidden
+
+    const expanded = Boolean(!this.hidden).toString()
+
+    this.#panel.setAttribute('aria-expanded', expanded)
+    this.#trigger.setAttribute('aria-expanded', expanded)
+    this.#panel.toggleAttribute('hidden')
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,9 @@
 import {getFeed} from "./feed";
 import RssFeed from "./models/RssFeed";
-
-const generateAccordionTrigger = (id : string) => {
-  const accordionTrigger = document.createElement('button')
-
-  accordionTrigger.classList.add('accordion-trigger')
-  accordionTrigger.id = `${id}-accordion`
-  accordionTrigger.setAttribute('aria-expanded', "true")
-  accordionTrigger.setAttribute('aria-controls', id)
-
-  return accordionTrigger
-}
-
-const generateAccordionPanel = (id : string) => {
-  const accordionPanel = document.createElement('div')
-
-  accordionPanel.classList.add('accordion-panel')
-  accordionPanel.id = id
-  accordionPanel.setAttribute('aria-expanded', "true")
-  accordionPanel.setAttribute('aria-labelledby', `${id}-accordion`)
-  // accordionPanel.toggleAttribute('hidden')
-  
-  return accordionPanel
-}
+import CustomAccordion from "./accordion"
 
 const generateRssFeedElement = (rssFeed : RssFeed) => {
   const newRssFeed = document.createElement('li')
-
-  const accordionHeader = document.createElement('h4')
-  accordionHeader.classList.add('accordion-header')
-
-  const accordionTrigger = generateAccordionTrigger(rssFeed.title)
-  const accordionPanel = generateAccordionPanel(rssFeed.title)
-
-  accordionTrigger.textContent = rssFeed.title
 
   // Need to map each item into a list
   if (rssFeed.items.length > 0) {
@@ -59,12 +29,9 @@ const generateRssFeedElement = (rssFeed : RssFeed) => {
       rssFeedItemsList.appendChild(rssFeedItemListContainer)
     })
 
-    accordionPanel.appendChild(rssFeedItemsList)
+    const accordion  = new CustomAccordion(rssFeed.title, rssFeedItemsList, false)
+    newRssFeed.appendChild(accordion.accordion)
   }
-
-  accordionHeader.appendChild(accordionTrigger)
-  newRssFeed.appendChild(accordionHeader)
-  newRssFeed.appendChild(accordionPanel)
 
   return newRssFeed
 }
@@ -81,29 +48,4 @@ rssFeeds.forEach(schema => {
   const rssFeed = new RssFeed(schema)
   const newRssElement = generateRssFeedElement(rssFeed)
   rssFeedList?.appendChild(newRssElement)
-})
-
-const accordionTriggers = document.querySelectorAll('.accordion-trigger')
-
-accordionTriggers.forEach(trigger => {
-    trigger.addEventListener('click', () => {
-        const accordionPanelId = trigger.getAttribute('aria-controls')
-
-        if (!accordionPanelId) {
-            console.error('Error: assign aria-controls on accordion trigger')
-            return
-        }
-
-        const accordionPanel = document.getElementById(accordionPanelId)
-
-        if (!accordionPanel) {
-            console.error(`Error: aria-controls for ${accordionPanelId} should have corresponding panel`)
-            return
-        }
-
-        const expanded = trigger.getAttribute('aria-expanded') === 'true'
-
-        accordionPanel.toggleAttribute('hidden', expanded)
-        trigger.setAttribute('aria-expanded', (!expanded).toString())
-    })
 })
