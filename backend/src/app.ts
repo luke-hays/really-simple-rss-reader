5e042326ab84821c69db985f1c9fff45d945b647
Luke Hays <lukehays32@gmail.com>
2024-07-12 16:10:09 -0400

    add comments to node files for later referencing

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,6 +5,9 @@ import routes from './routes/routes.js'
 const app = express()
 const PORT = 3001
 
+// Mddleware to enable CORS
+// Simple Usage for this project - more documentation here. 
+// https://expressjs.com/en/resources/middleware/cors.html
 app.use(cors())
 
 routes.forEach(({path, route}) => {
--- backend/src/routes/rss.ts
+++ backend/src/routes/rss.ts
@@ -1,7 +1,15 @@
 import express from 'express';
 
+// Can create a Router instance that can have middleware loaded and define routes
+// This can be exported and mounted on a path in our application
+// For now, these routes are consolidated in the routes.ts file
+// https://expressjs.com/en/guide/routing.html
 const router = express.Router();
 
+// We will define a more specific route in routes.ts
+// Asynchronous route handling and middleware require passing the error to next
+// Express will then catch them and can be handled with middleware
+// https://expressjs.com/en/guide/error-handling.html
 router.get('/', async (_, res, next) => {
   try {
     const response = await fetch('https://news.ycombinator.com/rss')
