0b3ff4f55b468cfc58f423b484cd78e7df4d4aa8
Luke Hays <lukehays32@gmail.com>
2024-07-19 16:19:32 -0400

    Update the previous CustomAccordion class to be a full web component utilizing a shadow dom.
    The original class was overly complex and awkward to parse and read. This new one is fairly simple, although it requires whatever code using it to dynamically altar the button trigger text and accordion content using slot elements.
    
    The reason for this change was my own personal interest in exploring how web components work. They seem like a powerful way to encapsulate shared HTML, but styling seems awkward, and I have ignore it for now.

--- frontend/src/components/accordion.ts
+++ frontend/src/components/accordion.ts
@@ -1,98 +1,59 @@
 // Spec according to W3 for practice
-// This may be transformed to a web component - for right now just rely on styling outside
-// TODO Look at web component feasability
-export default class CustomAccordion {
-  triggerContent : string | Node
-  panelContent : string | Node
-  hidden? : boolean = true // Collapsed would be a more a more appropriate term
-  accordion : HTMLDivElement = document.createElement('div')
-
-  #id : string
-  #header : HTMLHeadingElement = document.createElement('h4')
-  #trigger : HTMLButtonElement = document.createElement('button')
-  #panel : HTMLDivElement = document.createElement('div')
-
-  constructor(
-    triggerContent : string | Node, 
-    panelContent: string | Node, 
-    hidden? : boolean
-  ) {
-    this.#id = crypto.randomUUID()
-    this.triggerContent = triggerContent
-    this.panelContent = panelContent
-    this.hidden = hidden
-
-    console.log(panelContent)
-
-    this
-      .#createHeader()
-      .#createTrigger()
-      .#createPanel()
-
-    return this
+// Also testing out using web components with native API
+class CustomAccordion extends HTMLElement {
+  constructor() {
+    super()
   }
-
-  #createHeader() {
-    this.#header.classList.add('accordion-header')
-    this.accordion.appendChild(this.#header)
-
-    return this
-  }
-
-  #createTrigger() {
-    this.#trigger.classList.add('accordion-trigger')
-    this.#trigger.id = `${this.#id}-accordion`
-    this.#trigger.setAttribute('aria-controls', this.#id)
-    this.#trigger.setAttribute('aria-expanded', "true")
-
-    // Uncaught TypeError TypeError: Cannot read private member #panel from an object whose class did not declare it
-    // Probably due to passing it in as an event handler, bind this to properly call
-    this.#trigger.addEventListener('click', this.#toggleExpanded.bind(this))
-
-    if (typeof this.triggerContent === 'string') {
-      this.#trigger.textContent = this.triggerContent
-    } else {
-      this.#trigger.appendChild(this.triggerContent)
-    }
-
-    if (this.hidden) {
-      this.#trigger.setAttribute('aria-expanded', "false")
-    }
-
-    this.#header.appendChild(this.#trigger)
-
-    return this
-  }
-
-  #createPanel() {
-    this.#panel.classList.add('accordion-panel')
-    this.#panel.id = this.#id
-    this.#panel.setAttribute('aria-expanded', "true")
-    this.#panel.setAttribute('aria-labelledby', `${this.#id}-accordion`)
-
-    if (typeof this.panelContent === 'string') {
-      this.#panel.textContent = this.panelContent
-    } else {
-      this.#panel.appendChild(this.panelContent)
-    }
-
-    if (this.hidden) {
-      this.#panel.setAttribute('aria-expanded', "false")
-      this.#panel.toggleAttribute('hidden')
-    }
-
-    this.accordion.appendChild(this.#panel)
   
-    return this
-  }
+  // Define all functionality that element will have when connected to DOM
+  // Element attributes are not available in constructor, so need to be set up here
+  connectedCallback() {
+    // Create the shadow root, the custom element itself is the shadow host
+    const shadow = this.attachShadow({mode: 'open'})
 
-  #toggleExpanded() {
-    this.hidden = !this.hidden
+    const accordion = document.createElement('div')
+    const header = document.createElement('h4')
+    const trigger = document.createElement('button')
+    const content =  document.createElement('div')
 
-    const expanded = Boolean(!this.hidden).toString()
+    shadow.appendChild(accordion)
+    header.appendChild(trigger)
+    accordion.appendChild(header)
+    accordion.appendChild(content)
 
-    this.#panel.setAttribute('aria-expanded', expanded)
-    this.#trigger.setAttribute('aria-expanded', expanded)
-    this.#panel.toggleAttribute('hidden')
+    const id = crypto.randomUUID()
+
+    // Header
+    header.classList.add('class', 'accordion-header')
+
+    // Trigger
+    trigger.classList.add('class', 'accordion-trigger')
+    trigger.id = `${id}-accordion-trigger`
+    trigger.setAttribute('aria-controls', id)
+    trigger.setAttribute('aria-expanded', 'true')
+    trigger.innerHTML = `<slot name="accordion-trigger">Expand</slot>` // Slot is used for dynamic content
+
+    trigger.addEventListener('click', () => {
+      const currentlyExpanded = trigger.getAttribute('aria-expanded')
+      let newExpanded = 'true'
+
+      if (currentlyExpanded === 'true') {
+        newExpanded = 'false'
+      }
+
+      content.setAttribute('aria-expanded', newExpanded)
+      trigger.setAttribute('aria-expanded', newExpanded)
+      content.toggleAttribute('hidden')
+    })
+
+    // Content
+    content.classList.add('accordion-content')
+    content.id = id
+    content.setAttribute('aria-expanded', "true")
+    content.setAttribute('aria-labelledby', `${id}-accordion-trigger`)
+    content.innerHTML = `<slot name="accordion-content">Test</slot>` // Slot is used for dynamic content
   }
 }
+
+// Custom element needs to be registered
+customElements.define('custom-accordion', CustomAccordion)
