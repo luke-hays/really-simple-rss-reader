4f8fcc3047a6e4ff103edfa12075f6a338b08cc3
Luke Hays <lukehays32@gmail.com>
2024-07-19 16:24:33 -0400

    Update index.html to import accordion web component. Modify main script to use web component and clean up naming schemes.
    Building the Accordion in main was hard to read and the naming of variables was slightly confusing. Some reorganization and name changes, and some comments, make it clearer as to whats going on.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,41 +1,64 @@
-import CustomAccordion from "./components/accordion"
 import {getRssFeedList, addRssFeed} from './api/rss'
-import RssFeedElements from "./models/RssFeed"
 
-const generateRssFeedElement = (feed : RssFeedElements) => {
-  const rss = document.createElement('li')
-  const rssList = document.createElement('ul')
+// Grab the element responsible for rendering the list of feeds
+const rssAccordionList = document.querySelector('.rss-feed__list')
 
-  rss.classList.add('rss-feed__items-list')
+// If this is removed, just crash the page.
+// Ideally we would sound an alarm immediately in an actual prod environment
+if (rssAccordionList == null) throw new Error('Unable to build RSS list')
+
+const buildRssAccordion = (feed : RssFeedList) => {
+  const rssAccordionListElement = document.createElement('li')
+  const rssAccordion = document.createElement('custom-accordion')
+  const rssAccordionTitle = document.createElement('div')
+  const rssAccordionContent = document.createElement('ul')
+
+  // Title and Content of accordion is dynamic
+  rssAccordionTitle.setAttribute('slot', 'accordion-trigger')
+  rssAccordionContent.setAttribute('slot', 'accordion-content')
+
+  rssAccordion.appendChild(rssAccordionTitle)
+  rssAccordion.appendChild(rssAccordionContent)
+  rssAccordionListElement.appendChild(rssAccordion)
+
+  rssAccordionListElement.classList.add('rss-feed__items-list')
+  rssAccordionTitle.textContent = feed.title
 
   // For item in each feed we need to render a list of items
   feed.items.forEach(item => {
-    // itemList is our list element that will hold a content element
-    const itemList = document.createElement('li')
-    const itemContent = document.createElement('div')
-
-    itemContent.classList.add('rss-feed__list-item-content')
-
-    const {title, source} = item
+    const itemListElement = document.createElement('li')
     const itemTitle = document.createElement('a')
+    const itemContent = document.createElement('div')
     const itemDescription = document.createElement('div')
-    
-    itemTitle.textContent = title
-    itemTitle.setAttribute('href', source)
-    
+
+    itemListElement.appendChild(itemContent)
     itemContent.appendChild(itemTitle)
     itemContent.appendChild(itemDescription)
+    rssAccordionContent.appendChild(itemListElement)
 
-    // We need to append the content to the new list element
-    itemList.appendChild(itemContent)
-    // Then append this list element to an unordered list of rss feeds
-    rssList.appendChild(itemList)
+    const {title, source} = item
+
+    itemContent.classList.add('rss-feed__list-item-content')
+    itemTitle.textContent = title
+    itemTitle.setAttribute('href', source)
   })
 
-  const rssAccordion = new CustomAccordion(feed.title, rssList, false)
-  
-  rss.appendChild(rssAccordion.accordion)
-  return rss
+  return rssAccordionListElement
+}
+
+const populateRssFeedList = async () => {
+  // Fetch a collection of RSS Feeds from a backend service
+  // Right now this will just be one
+  const rssList = await getRssFeedList()
+
+  // Need to handle possible undefined or null values
+  if (rssList == null) return
+
+  // Loop through and append to the main list on the page
+  rssList.forEach(async feed => {
+    const accordion = buildRssAccordion(feed)
+    rssAccordionList.appendChild(accordion)
+  })
 }
 
 const addFeed = async () => {
@@ -51,39 +74,19 @@ const addFeed = async () => {
     return
   }
 
-  const rssFeedList = document.querySelector('.rss-feed__list')
-
   try {
     const newRssFeed = await addRssFeed(url)
 
     if (newRssFeed == null) throw new Error('handle later')
 
-    const newRssElement = generateRssFeedElement(newRssFeed)
+    const newRssAccordion = buildRssAccordion(newRssFeed)
 
-    rssFeedList?.appendChild(newRssElement)
+    rssAccordionList.appendChild(newRssAccordion)
 
-    input.value = ''
+    input.value = '' // TODO Need to clear this in more states
   } catch (error: any) {
     console.error(error) // TODO Error state
   }
-}
-
-const populateRssFeedList = async () => {
-  // Fetch a collection of RSS Feeds from a backend service
-  // Right now this will just be one
-  const rssFeedList = await getRssFeedList()
-
-  // Need to handle possible undefined or null values
-  if (rssFeedList == null) return
-  
-  // Grab the element responsible for rendering the list of feeds
-  const rssContainer = document.querySelector('.rss-feed__list')
-
-  // Loop through and append to the main list on the page
-  rssFeedList.forEach(async feed => {
-    const rss = generateRssFeedElement(feed)
-    rssContainer?.appendChild(rss)
-  })
 }
 
 await populateRssFeedList()
