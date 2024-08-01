c1e963c4cef458f8239fb2d6c609da5635cd72ae
Luke Hays <lukehays32@gmail.com>
2024-07-31 20:19:41 -0400

    Update API to utilize W3 Feed Validator Service
    This adds a check in to validate a source against W3's feed validation service.
    
    Updated some comments and updated shared code.

--- backend/src/routes/rss.ts
+++ backend/src/routes/rss.ts
@@ -3,35 +3,19 @@ import {JSDOM} from 'jsdom';
 import {ObjectId} from 'mongodb';
 import {CacheClient} from '../cache/client';
 
+// Parsing tool will be needed across functions
+const dom = new JSDOM()
+const parser = new dom.window.DOMParser()
+
+// Create a Router instance that can have middleware loaded and define routes
+// This can be exported and mounted on a path in our application
+// For now, these routes are consolidated in the routes.ts file
+// https://expressjs.com/en/guide/routing.html
+const router = express.Router();
+const dbConfig = {dbName: 'rss', collectionName: 'feeds'}
+
 const queryRssDocument = (rssFeed : Document | Element, selector : string) => {
   return rssFeed.querySelector(selector)?.textContent?.trim() ?? ''
-}
-
-// Node is not going to have native browser api's to help, so we are going to rely on JSDOM
-// What we really need is the DOMParser class
-// We'll get a document we can select elements on by using the parseFromString method
-const parseRss = (rssDocString : string) => {
-  const dom = new JSDOM()
-  const parser = new dom.window.DOMParser()
-  const document = parser.parseFromString(rssDocString, 'application/xhtml+xml')
-
-  if (document.querySelector('parseError')) throw new Error('Unable to parse doc string')
-
-  const title = document.querySelector('title')?.textContent
-  const description = document.querySelector('description')?.textContent
-  const ttl = document.querySelector('ttl')?.textContent
-
-  const items : Array<{title: string, description: string, source: string}> = []
-
-  document.querySelectorAll('item').forEach(item => {
-    items.push({
-      title: queryRssDocument(item, 'title'),
-      description: queryRssDocument(item, 'description'),
-      source: queryRssDocument(item, 'link')
-    })
-  })
-
-  return {title, description, ttl, items}
 }
 
 const getCacheExpirationTime = (cacheControl : string | null, rssTtl? : string | null) => {
@@ -57,6 +41,51 @@ const getCacheExpirationTime = (cacheControl : string | null, rssTtl? : string |
   return expiration
 }
 
+// Node is not going to have native browser api's to help, so we are going to rely on JSDOM
+// What we really need is the DOMParser class
+// We'll get a document we can select elements on by using the parseFromString method
+const parseRss = async (source : string) => {  
+  const rssDoc = await fetch(source)
+  const body = await rssDoc.text()
+
+  const document = parser.parseFromString(body, 'application/xhtml+xml')
+
+  if (document.querySelector('parseError')) throw new Error('Unable to parse doc string')
+
+  const title = document.querySelector('title')?.textContent
+  const description = document.querySelector('description')?.textContent
+  const ttl = document.querySelector('ttl')?.textContent
+
+  const items : Array<{title: string, description: string, source: string}> = []
+
+  document.querySelectorAll('item').forEach(item => {
+    items.push({
+      title: queryRssDocument(item, 'title'),
+      description: queryRssDocument(item, 'description'),
+      source: queryRssDocument(item, 'link')
+    })
+  })
+
+  const expiration = getCacheExpirationTime(rssDoc.headers.get('cache-control'), ttl)
+
+  return {title, description, expiration, items}
+}
+
+// Send requests to the W3 feed validator and receive a response in SOAP 1.2
+// RSS Feed validator maintainance does not seem to be a thing. Could look into alternatives
+// Trying to set up self hosting for this failed for a myriad of reasons - although it would be preferable
+// https://validator.w3.org/feed/check.cgi?output=soap12&url=https://news.ycombinator.com/rss
+// https://validator.w3.org/feed/docs/soap.html#libraries
+const validateRssUrl = async (source : string) => {
+  const response = await fetch(`https://validator.w3.org/feed/check.cgi?output=soap12&url=${source}`)
+  const validationDoc = await response.text()
+
+  const document = parser.parseFromString(validationDoc, 'application/xml')
+  const errorCount = document.querySelector('errorcount')
+
+  return errorCount?.textContent === '0' // Any errors should invalidate the check
+}
+
 // Cache the new rss feed's items
 // Expiration time should be set using either the Cache Control header from our response OR the TTL element from the rss doc
 // If these fail the default fallback is set to an hour
@@ -64,14 +93,7 @@ const addFeedToCache = async (cache : CacheClient, key : string, value : string,
     await cache.set(key, value, {EX: ttl})
 }
 
-// Can create a Router instance that can have middleware loaded and define routes
-// This can be exported and mounted on a path in our application
-// For now, these routes are consolidated in the routes.ts file
-// https://expressjs.com/en/guide/routing.html
-const router = express.Router();
-const dbConfig = {dbName: 'rss', collectionName: 'feeds'}
-
-// We will define a more specific route in routes.ts
+// Define a more specific route in routes.ts
 // Asynchronous route handling and middleware require passing the error to next
 // Express will then catch them and can be handled with middleware
 // https://expressjs.com/en/guide/error-handling.html
@@ -111,12 +133,7 @@ router.get('/:id', async (req, res, next) => {
   
     if (rss == null) throw new Error('Unable to find record with id')
 
-    const rssDoc = await fetch(rss.source)
-    const body = await rssDoc.text()
-
-    const {items, ttl} = parseRss(body)
-
-    const expiration = getCacheExpirationTime(rssDoc.headers.get('cache-control'), ttl)
+    const {items, expiration} = await parseRss(rss.source)
     
     // Run the cache strategy again after fetching items for the feed
     await addFeedToCache(req.cache, req.params.id, JSON.stringify(items), expiration)
@@ -133,22 +150,17 @@ router.post('/', async (req, res, next) => {
     // Check if valid source. Insert into table if valid
     // https://cheatsheetseries.owasp.org/cheatsheets/Input_Validation_Cheat_Sheet.html
     // This is the only input provided, and in a real world scenario needs to be locked down tight
-    
-    // TODO Validate Source https://validator.w3.org/feed/docs/soap.html
     const source = req.body['source']
-    
-    // If the source looks legit, we need to fetch some data from it to add to the database, like a title and description
-    const rssDoc = await fetch(source)
 
-    const body = await rssDoc.text()
+    const validUrl = await validateRssUrl(source)
 
-    const {title, description, items, ttl} = parseRss(body)
+    if (!validUrl) throw new Error('Invalid RSS format')
+
+    const {title, description, items, expiration} = await parseRss(source)
 
     const newRssFeed = {title, description, source}
 
     const record = await req.db.insertRecord(dbConfig, newRssFeed)
-
-    const expiration = getCacheExpirationTime(rssDoc.headers.get('cache-control'), ttl)
 
     await addFeedToCache(req.cache, record.insertedId.toString(), JSON.stringify(items), expiration)
 
